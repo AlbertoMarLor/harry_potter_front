e7d7fcb05f2e2c08a26e82c1721ca26eea657f3a
AlbertoMarLor <amartinl92@gmail.com>
2023-04-03 19:19:23 +0200

    1er commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesComponent } from './components/movies/movies.component';
+import { CharactersComponent } from './components/characters/characters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', pathMatch: 'full', redirectTo: '/characters' },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'characters', component: CharactersComponent },
+  { path: '**', redirectTo: '/characters' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
